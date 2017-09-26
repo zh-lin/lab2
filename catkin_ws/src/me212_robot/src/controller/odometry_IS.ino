b0ce32154571f422958370667e1b58c77384fae9
zh-lin <zhlin479@gmail.com>
2017-09-26 10:03:12 +0100

    test

--- catkin_ws/src/me212_robot/src/controller/odometry_IS.ino
+++ catkin_ws/src/me212_robot/src/controller/odometry_IS.ino
@@ -42,9 +42,11 @@ void loop() {
   //////////////////////////////////////////////////////////////////
   // write your code
 
-  // theta = ???
-  // x = ??? 
-  // y = ???
+   d_theta = (dis_R-dis_L)/WIDTH;
+   theta = d_theta + theta;
+
+   x = x + (cos(theta)*dis_R+cos(theta)*dis_L)/2;
+   y = y + (sin(theta)+dis_R+sin(theta)*dis_L)/2;
 
   ///////////////////////////////////////////////////////////////////
   msg.data_length = 3;
